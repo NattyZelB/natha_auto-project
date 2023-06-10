3b3b8126207e3b1b5f687d395033324220dd675f
NattyZelB <92927774+NattyZelB@users.noreply.github.com>
2023-06-10 22:38:49 +0200

    add pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contract', views.contract, name='contract'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contract(request):
+    return render(request, 'pages/contract.html')
