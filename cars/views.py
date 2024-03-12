6923aef0f49047c899942831a2cd6349d98b8221
NattyZelB <92927774+NattyZelB@users.noreply.github.com>
2024-03-12 18:21:23 +0100

    Edit python version 3.10.11 in runtiome.txt

--- cars/views.py
+++ cars/views.py
@@ -23,10 +23,10 @@ def cars(request):
     }
     return render(request, 'cars/cars.html', data)
 
-def car_detail(request,id):
+def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
     data = {
-       'single_car': single_car
+       'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html', data)
 def search(request):
